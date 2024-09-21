1d4cb0c033a51c766fb9ad0ad9a3a7b31da8e198
mscha <m.saleh.sedeghi@gmail.com>
2024-09-21 16:27:57 +0330

    compose added to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
 }
 
 android {
@@ -31,10 +32,25 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+    composeOptions{
+        kotlinCompilerExtensionVersion = "1.5.0"
+    }
+    kotlinOptions{
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.material:material")
+    implementation ("androidx.compose.runtime:runtime")
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation (platform("androidx.compose:compose-bom:2022.10.00"))
+    implementation ("androidx.compose.ui:ui-graphics")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
 
     implementation(libs.appcompat)
     implementation(libs.material)
---
+++ app/src/main/java/com/example/myapplication/SettingActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_setting)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val composeView = findViewById<ComposeView>(R.id.composeView)
+        composeView.setContent {
+            // Compose code here
+
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun ShowTextView(){
+        Text("Hello from Compose!")
+    }
+
+}
+
+
